1de664c2af6a552e069d6f6985ab944e74bdd917
Syarif784 <144253104+syarifgithub784@users.noreply.github.com>
2024-05-21 21:47:05 +0700

    keliling

--- js/script.js
+++ js/script.js
@@ -3,12 +3,25 @@ document.getElementById('luassegitiga').addEventListener('submit', function(even
     const alas = parseFloat(document.getElementById('alas').value);
     const tinggi = parseFloat(document.getElementById('tinggi').value);
 
-    if (!alas || !tinggi) {
-        document.getElementById('hasil').textContent = 'Silakan masukkan angka yang valid untuk alas dan tinggi.';
-        return;
-    }
 
     const luas = 0.5 * alas * tinggi;
+    document.getElementById('hasilluas').textContent = 'Luas segitiga: ' + luas.toFixed(2);
 
-    document.getElementById('hasil').textContent = 'Luas segitiga: ' + luas.toFixed(2);
+});
+
+
+document.getElementById('kelilingsegitiga').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var sisi1 = parseFloat(document.getElementById('sisi1').value);
+    var sisi2 = parseFloat(document.getElementById('sisi2').value);
+    var sisi3 = parseFloat(document.getElementById('sisi3').value);
+
+    var keliling = sisi1 + sisi2 + sisi3;
+
+    document.getElementById('hasilkeliling').innerHTML = '<p>Keliling Segitiga: ' + keliling.toFixed(2) + '</p>';
+});
+
+document.getElementById('kelilingsegitiga').addEventListener('reset', function() {
+    document.getElementById('hasilkeliling').innerHTML = '';
 });
